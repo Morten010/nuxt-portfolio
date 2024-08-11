922b5882f56ba074c379b04f8654d1d0e97e4206
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-08-11 06:33:05 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss"
   ],
   icon: {
-    // provider: 'server',
+    provider: 'server',
     customCollections: [
       {
         prefix: 'self',
