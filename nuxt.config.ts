24cda720098c33f9b61d1f2af1937db273412135
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-08-12 08:23:00 +0200

    add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  plugins: [
+    '~/plugins/analytics.client.ts'
+  ],
   modules: [
     '@nuxtjs/i18n',
     "@nuxt/fonts",
