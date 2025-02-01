17dac8c198b0de1abf5e166f01e0b174a23eea18
Morten010 <97233551+Morten010@users.noreply.github.com>
2025-02-01 20:33:40 +0100

    start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,68 +1,65 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/i18n',
+    "@nuxtjs/i18n",
     "@nuxt/fonts",
     "@nuxt/content",
     "@nuxt/image",
     "@nuxt/icon",
     "@nuxtjs/tailwindcss",
     "@nuxtjs/seo",
-    '@vueuse/motion/nuxt'
+    "@vueuse/motion/nuxt",
   ],
   icon: {
     customCollections: [
       {
-        prefix: 'self',
-        dir: './assets/custom-icons'
+        prefix: "self",
+        dir: "./assets/custom-icons",
       },
     ],
   },
   site: {
-    url: 'https://www.mortenra.com/',
-    name: 'Morten Rasmussen',
-    description: 'I am a webdeveloper from Kolding, Denmark. With great interest in full stack web apps and websites. I love to make things and see how things just quietly fall into place.',
-
+    url: "https://www.mortenra.com/",
+    name: "Morten Rasmussen",
+    description:
+      "I am a webdeveloper from Kolding, Denmark. With great interest in full stack web apps and websites. I love to make things and see how things just quietly fall into place.",
   },
   content: {
-    locales:[
-      'en', 'da'
-    ],
-    
+    locales: ["en", "da"],
   },
   tailwindcss: {
     config: {
       theme: {
         extend: {
           backgroundImage: {
-            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
           },
-        }
-      }
-    }
+        },
+      },
+    },
   },
   i18n: {
-    strategy: 'no_prefix',
-    vueI18n: './i18n.config.ts',
-    baseUrl: 'https://www.mortenra.com/',
+    strategy: "no_prefix",
+    vueI18n: "./i18n.config.ts",
+    baseUrl: "https://www.mortenra.com/",
     detectBrowserLanguage: {
       useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root',  // recommended
+      cookieKey: "i18n_redirected",
+      redirectOn: "root", // recommended
     },
     locales: [
       {
-        code: 'en',
-        iso: 'en-US',
-        name: 'english'
+        code: "en",
+        iso: "en-US",
+        name: "english",
       },
       {
-        code: 'da',
-        iso: 'da-DK',
-        name: 'danish'
-      }
+        code: "da",
+        iso: "da-DK",
+        name: "danish",
+      },
     ],
   },
-})
+});
